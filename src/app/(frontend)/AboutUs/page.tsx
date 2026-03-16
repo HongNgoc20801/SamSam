0dc6eec6c792b960a6b474bdf1c4d84d9df706f5
HongNgoc20801 <hongngoc20082001@gmail.com>
2026-03-16 16:45:09 +0100

    make AboutUs page dynamic

--- src/app/(frontend)/AboutUs/page.tsx
+++ src/app/(frontend)/AboutUs/page.tsx
@@ -2,6 +2,8 @@ import AboutUsContent from "../components/AboutUspage/AboutUsContent";
 import FooterWelcome from "../components/landing/Footer/Footer";
 import NavbarWelcome from "../components/landing/Navigation/Navigation";
 
+export const dynamic = 'force-dynamic'
+
 export default function Page() {
   return (
     <>

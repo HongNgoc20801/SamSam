a6576d61c63e9f38bb31644670d64063034c6450
HongNgoc20801 <hongngoc20082001@gmail.com>
2026-03-16 23:55:48 +0100

    fix next standalone config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { withPayload } from '@payloadcms/next/withPayload'
 
-/** @type {import('next').NextConfig} */
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const nextConfig = {
-  // Your Next.js config here
+  output: 'standalone',
+  outputFileTracingRoot: __dirname,
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
